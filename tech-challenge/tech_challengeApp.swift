f0dd0fdc8873d1a33194c04da0b60a8dccb2bff2
Jasmine Patel <jasmine.patel@footasylum.com>
2023-08-25 15:43:53 +0100

    Initial Commit

---
+++ tech-challenge/tech_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tech_challengeApp.swift
+//  tech-challenge
+//
+//  Created by Jasmine Patel on 25/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct tech_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
